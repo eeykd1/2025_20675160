82d9622e2d15edf791782f1e2aae311b78248693
Keene <eeykd1@nottingham.ac.uk>
2026-02-13 15:58:24 +0000

    Add Doxygen documentation to adder.h

---
+++ Worksheet5/adder.h
@@ -0,0 +1,19 @@
+/**
+ * @file adder.h
+ * @author Your Name
+ * @brief A simple library for mathematical addition.
+ */
+
+#ifndef ADDER_H
+#define ADDER_H
+
+/**
+ * @brief Adds two integers together.
+ * * This function takes two integer inputs and returns their sum.
+ * * @param a The first integer to add.
+ * @param b The second integer to add.
+ * @return The sum of a and b.
+ */
+int add(int a, int b);
+
+#endif
